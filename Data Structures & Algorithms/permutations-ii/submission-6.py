ba74315ba0e1b63b4b06b9eb4a9998a39fdaf902
Yakov Mishayev <63517398+YakovM93@users.noreply.github.com>
2026-04-10 20:45:24 +0300

    Add: permutations-ii - submission-6

---
+++ Data Structures & Algorithms/permutations-ii/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        perms = [[]]
+        for n in nums:
+            new_perms = []
+            for p in perms:
+                for i in range(len(p)+1):
+                    p_copy = p.copy()
+                    p_copy.insert(i, n)
+                    if p_copy not in new_perms:
+                        new_perms.append(p_copy)
+                    #if i < len(p) and p[i] == n: break
+            perms = new_perms
+
+        return perms
